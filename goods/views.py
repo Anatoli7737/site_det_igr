533b167f21518bd8ab09e1e5daa1a194998eaddd
Anatoli7737 <160400073+Anatoli7737@users.noreply.github.com>
2024-05-20 21:34:55 +0300

    readme/search

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,14 @@
+from unicodedata import category
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from goods.models import Products
+from goods.utils import q_search
 
 
 def cataloge(request):
+
+    query = request.GET.get("q", None)
 
     goods = Products.objects.all()
 
